e5b96d4f52b111fe054835e058e247cb7dfb7fa6
Tim van de Vathorst <tim@vandevathorst.nl>
2016-10-22 12:09:13 +0200

    Added flow

--- app.js
+++ app.js
@@ -2,31 +2,35 @@
 
 var http = require('http');
 
-function init()
-{	
-        Homey.manager('speech-input').on('speech', function(speech)
-        {
-            http.get('http://catfacts-api.appspot.com/api/facts', function(res)
-            {
-                var body = '';
-                
-                res
-                    .on('data', function(chunk)
-                    {
-                        body += chunk;
-                    })
-                    .on('end', function()
-                    {
-                        body = JSON.parse(body);
-                        Homey.manager('speech-output').say( body.facts[0] );
-                    });
-                    
-            }).on('error', function(e)
-            {
-                console.log("Got error: " + e.message);
-                Homey.manager('speech-output').say('No internet connection');
-            });
-        });
+module.exports.init = function() {
+    Homey.manager('speech-input').on('speech', function(speech, callback) {
+        getRandomCatFact();
+    });   
 }
 
-module.exports.init = init;
+Homey.manager('flow').on('action.tell_random_cat_fact', function( callback, args ) {
+    getRandomCatFact();      
+    callback( null, true );
+});
+
+function getRandomCatFact() {
+    http.get('http://catfacts-api.appspot.com/api/facts', function(res) {
+        var body = '';
+
+        res
+            .on('data', function(chunk)
+            {
+                body += chunk;
+            })
+            .on('end', function()
+            {
+                body = JSON.parse(body);
+                Homey.manager('speech-output').say( body.facts[0] );
+            });
+            
+    }).on('error', function(e)
+    {
+        console.log("Got error: " + e.message);
+        Homey.manager('speech-output').say('No internet connection');
+    });
+}
